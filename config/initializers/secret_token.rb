8398d48ede5fdc9c5aa3966f1a5c59c8846f4399
ajahongir <ajahongir@ajahongir-ENVY.(none)>
2012-02-25 23:20:12 +0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Piyola::Application.config.secret_token = 'e4c2ae9e7b8f2ada3b488e40177699775308fe03752623907f5acee4551ea3099b1246b9867b978c67d7f41fda67f2758535ff84ae9b9286909cebfd6b8be716'
